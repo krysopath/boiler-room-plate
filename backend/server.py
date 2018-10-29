8b3d9d1cf4138689f5e40ed3db127bb9cfad2600
Georg vom Endt <gve@3yourmind.com>
2018-10-29 21:14:44 +0100

    added functional people api
    Why:
    - poc for backend
    
    Goal:
    - implement endpoints for the frontend
    
    Signed-off-by: Georg vom Endt <gve@3yourmind.com>

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,75 @@
 #!/usr/bin/env python3
-from flask import Flask
-from sys import argv
-import os
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import sys
+from math import pi
 
 app = Flask(__name__)
 
-BIND = argv[1]
-PORT= argv[2]
+BIND = sys.argv[1]
+PORT = sys.argv[2]
 
-@app.route("/")
-def bait(): 
-    return app.send_static_file('index.html')
+app = Flask(__name__)
+api = Api(app)
 
-if __name__ == "__main__":  
-    app.run(host=BIND,port=PORT,debug=True)
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('occupation')
+parser.add_argument('age')
+parser.add_argument('reason')
+
+
+class People(Resource):
+    def get(self, human_id):
+        return PEOPLE[human_id]
+
+    def put(self, human_id):
+        # Fishy Comment:
+        # lol post form data like this but not as api for js frontend please
+        # research application/x-www-form-urlencoded!
+        PEOPLE[human_id] = request.form['data']
+        return {human_id: PEOPLE[human_id]}
+
+    def delete(self, human_id):
+        try:
+            del PEOPLE[human_id]
+            return {"status": "OK"}, 204
+        except KeyError as ke:
+            return {"status": "No such resource"}, 404
+
+
+class PeopleList(Resource):
+    def get(self):
+        return PEOPLE
+
+    def post(self):
+        args = parser.parse_args()
+        human_id = len(PEOPLE.keys())+1
+        PEOPLE[human_id] = {
+            "name": args["name"],
+            "occupation": args["occupation"],
+            "age": args["age"],
+            "reason": args["reason"]
+        }
+
+        return PEOPLE[human_id], 201
+
+
+api.add_resource(PeopleList, '/people')
+api.add_resource(People, '/people/<int:human_id>')
+
+
+PEOPLE = {
+    0: {"name": "Dennis Nuesser",
+        "occupation": "currently all sorts",
+        "age": pi,
+        "reason": "since he is awesome"},
+}
+
+if __name__ == "__main__":
+    app.run(
+        host=BIND,
+        port=PORT,
+        debug=True
+    )
 
